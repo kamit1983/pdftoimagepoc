b8dfa50d2558f996232b790d04b311b556668c54
Amit Kumar <kamit1983@gmail.com>
2019-01-24 11:01:34 +0530

    works 2

--- pdftoimageserver/routes/index.js
+++ pdftoimageserver/routes/index.js
@@ -40,7 +40,43 @@
               const browser = await puppeteer.launch({executablePath: '../../chrome-mac/Chromium.app/Contents/MacOS/Chromium'});
               const page = await browser.newPage();
               await page.goto('http://localhost:8000/public/pdf.html');
-              const imageUrls = await page.evaluate(() => showPDF())
+              //console.log('html',html);
+              //await page.setContent(`data:text/html,${html}`,{ waitUntil: 'networkidle2' });
+
+              let pdf_url = "http://localhost:8000/tmp/input.pdf";
+              const imageUrls = await page.evaluate(async ({pdf_url}) => {
+                let data = [];
+                 
+                  const pdf_doc = await PDFJS.getDocument({ url: pdf_url });
+                    var __PDF_DOC = pdf_doc;
+                    var __TOTAL_PAGES = __PDF_DOC.numPages;
+                    
+                      for(let i=1; i<= __TOTAL_PAGES ; i++){
+                        // Fetch the page
+                        const page = await __PDF_DOC.getPage(i);
+                        $('<canvas />', { id: `pdf-canvas${i}` }).appendTo('body');
+                        __CANVAS = $(`#pdf-canvas${i}`).get(0),
+                        __CANVAS_CTX = __CANVAS.getContext('2d');
+                        // As the canvas is of a fixed width we need to set the scale of the viewport accordingly
+                        var scale_required = __CANVAS.width / page.getViewport(1).width;
+
+                        // Get viewport of the page at required scale
+                        var viewport = page.getViewport(scale_required);
+
+                        // Set canvas height
+                        __CANVAS.height = viewport.height;
+
+                        var renderContext = {
+                          canvasContext: __CANVAS_CTX,
+                          viewport: viewport
+                        };
+                        
+                        // Render the page contents in the canvas
+                        await page.render(renderContext)
+                        data.push($(`#pdf-canvas${i}`).get(0).toDataURL());
+                      }
+                      return data;
+              },{pdf_url});
               const urls = download(imageUrls,`${file.name.split('.')[0]}`);
               console.log(urls);
               await browser.close();
@@ -59,6 +95,21 @@
             }
             return urls;
           }
+          var html = `<!doctype html>
+          <html lang="en">
+          <head>
+            <meta charset="utf-8">
+            <title>Pdftoimageclient</title>
+            <base href="/">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+            <script src="http://localhost:8000/public/pdf.js"></script>
+            <script src="http://localhost:8000/public/pdf.worker.js"></script>
+          </head>
+          <body>
+             
+          </body>
+          </html>`;
       });
     };
   })();
